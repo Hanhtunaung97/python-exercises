8b031dbeb8487c15495986fef85b766703d7f639
Hanhtunaung97 <littlehanhtunaung97@gmail.com>
2018-06-09 16:37:57 +0630

    python 16

--- ex1.py
+++ ex1.py
@@ -1,7 +1,3 @@
-print("Hello World")
-print("Hello again")
-print("I like typing this")
-print("This is fun.")
-print('Yay! Printing.')
-print("I'd much rather you 'not'.")
-print('I "said" do not touch this.')
+why erase??
+so I can't find that file????
+line3
---
+++ ex16.py
@@ -0,0 +1,31 @@
+from sys import argv
+
+script, filename = argv
+
+print(f"we're going to erase {filename}.")
+print("if you don't want that,hit CTRL -C (^C).")
+print("if you do want that, hit RETURN.")
+
+input("?")
+print("Opening the file...")
+target =open(filename, 'w')
+print("Truncating the file. Goodbye!")
+target.truncate()
+
+print("Now I'm going to ask you for three lines.")
+
+line1 = input("line1: ")
+line2 = input("line2: ")
+line3 = input("line3: ")
+
+print("I'm going to write these to the file.")
+
+target.write(line1)
+target.write("\n")
+target.write(line2)
+target.write("\n")
+target.write("line3")
+target.wirte("\n")
+
+print("And finally, we close it.")
+target,close()
