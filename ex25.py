c66583aca27ce9a5f8390ca3e6fec51e536e8e95
Hanhtunaung97 <littlehanhtunaung97@gmail.com>
2018-06-10 13:09:21 +0630

    python ex25.py

---
+++ ex25.py
@@ -0,0 +1,132 @@
+def break_words(stuff):
+    """This function will break up words for us."""
+    words = stuff.split(' ')
+    return words
+def sort_words(words):
+    """Sorts the words."""
+    return sorted(words)
+def print_first_word(words):
+    """Prints the first word after popping it off."""
+    word = words.pop(0)
+    print(word)
+def print_last_word(words):
+    """Prints the last word after popping it off."""
+    word = words.pop(-1)
+    print(word)
+
+def sort_sentence(sentence):
+    """Takes in a full sentence and return the sorted words."""
+def sort_sentence(sentence):
+def sort_sentence(sentence)    
+
+    words = break_words(sentence)
+    reutrn sort_words(words)
+def print_first_and_last(sentence):
+    """Prints the first and last words of the sentence."""
+    """print the first and last words of the sentence."""
+    """PRINTS the first and last words of the senctence."""
+    words = break_words(sentence)
+    print_first_word(words)
+    print_last_word(words)
+
+def print_first_and_last_sorted(sentence):
+    """sorrts the words then printthe first and last one."""
+    words = sort_sentence(sentence)
+    print_first_word(words)
+    print_last_word(words)
+áŠ áˆ›áˆ­áŠ›
+ÐÒ§ÑÑˆÓ™Ð°
+Ø§Ù„Ø¹Ø±Ø¨ÙŠØ©
+AragonÃ©s
+Arpetan
+AzÉ™rbaycanca
+Bamanankan
+à¦¬à¦¾à¦‚à¦²à¦¾
+BÃ¢n-lÃ¢m-gÃº
+Ð‘ÐµÐ»Ð°Ñ€ÑƒÑÐºÐ°Ñ
+Ð‘ÑŠÐ»Ð³Ð°Ñ€ÑÐºÐ¸
+Boarisch
+Bosanski
+Ð‘ÑƒÑ€ÑÐ°Ð´
+CatalÃ 
+Ð§Ó‘Ð²Ð°ÑˆÐ»Ð°
+ÄŒeÅ¡tina
+Cymraeg
+Dansk
+Deutsch
+Eesti
+Î•Î»Î»Î·Î½Î¹ÎºÎ¬
+EspaÃ±ol
+Esperanto
+ÙØ§Ø±Ø³ÛŒ
+FranÃ§ais
+Frysk
+Gaelg
+GÃ idhlig
+Galego
+í•œêµ­ì–´
+Õ€Õ¡ÕµÕ¥Ö€Õ¥Õ¶
+à¤¹à¤¿à¤¨à¥à¤¦à¥€
+Hrvatski
+Ido
+Interlingua
+Italiano
+×¢×‘×¨×™×ª
+à²•à²¨à³à²¨à²¡
+Kapampangan
+áƒ¥áƒáƒ áƒ—áƒ£áƒšáƒ˜
+ÒšÐ°Ð·Ð°Ò›ÑˆÐ°
+KreyÃ²l ayisyen
+LatgaÄ¼u
+Latina
+LatvieÅ¡u
+LÃ«tzebuergesch
+LietuviÅ³
+Magyar
+ÐœÐ°ÐºÐµÐ´Ð¾Ð½ÑÐºÐ¸
+Malti
+à¤®à¤°à¤¾à¤ à¥€
+áƒ›áƒáƒ áƒ’áƒáƒšáƒ£áƒ áƒ˜
+Ù…Ø§Ø²ÙØ±ÙˆÙ†ÛŒ
+Bahasa Melayu
+ÐœÐ¾Ð½Ð³Ð¾Ð»
+Nederlands
+à¤¨à¥‡à¤ªà¤¾à¤² à¤­à¤¾à¤·à¤¾
+æ—¥æœ¬èªž
+Norsk bokmÃ¥l
+Nouormand
+Occitan
+OÊ»zbekcha/ÑžÐ·Ð±ÐµÐºÑ‡Ð°
+à¨ªà©°à¨œà¨¾à¨¬à©€
+Ù¾Ù†Ø¬Ø§Ø¨ÛŒ
+Ù¾ÚšØªÙˆ
+PlattdÃ¼Ã¼tsch
+Polski
+PortuguÃªs
+RomÃ¢nÄƒ
+Romani
+Ð ÑƒÑÑÐºÐ¸Ð¹
+Seeltersk
+Shqip
+Simple English
+SlovenÄina
+Ú©ÙˆØ±Ø¯ÛŒÛŒ Ù†Ø§ÙˆÛ•Ù†Ø¯ÛŒ
+Ð¡Ñ€Ð¿ÑÐºÐ¸ / srpski
+Suomi
+Svenska
+Tagalog
+à®¤à®®à®¿à®´à¯
+à¸ à¸²à¸©à¸²à¹„à¸—à¸¢
+Taqbaylit
+Ð¢Ð°Ñ‚Ð°Ñ€Ñ‡Ð°/tatarÃ§a
+à°¤à±†à°²à±à°—à±
+Ð¢Ð¾Ò·Ð¸ÐºÓ£
+TÃ¼rkÃ§e
+Ð£ÐºÑ€Ð°Ñ—Ð½ÑÑŒÐºÐ°
+Ø§Ø±Ø¯Ùˆ
+Tiáº¿ng Viá»‡t
+VÃµro
+æ–‡è¨€
+å´è¯­
+×™×™Ö´×“×™×©
+ä¸­æ–‡
